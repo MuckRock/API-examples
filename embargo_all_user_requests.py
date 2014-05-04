79f70d4c72f259ce9ed3fc713bc4dcb8432ba067
Michael Morisy <morisy@gmail.com>
2014-05-04 14:03:52 -0400

    Adding new embargo script.

---
+++ embargo_all_user_requests.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -- coding: utf-8 --
+import requests
+import json
+
+token = '' # You'll need a token. Get here: https://github.com/MuckRock/API-examples/blob/master/get_API_key.py
+url = 'https://www.muckrock.com/api_v1/'
+
+
+headers = {'Authorization': 'Token %s' % token, 'content-type': 'application/json'}
+
+
+user = "morisy" #<--- Put username here. Case sensitive.
+
+page = 1
+next_ = url+"foia/?user="+user
+
+
+
+while next_ is not None:
+    r = requests.get(next_, headers=headers)
+    try:
+        json_data = r.json()
+        print 'Page %d of %d' % (page, json_data['count'] / 20 + 1)
+
+        next_ = json_data['next']
+        for request in json_data['results']:
+            reqNumber = request["id"]
+            editedRequest = requests.get(url + 'foia/%s/' % str(reqNumber), headers=headers)
+            print "Embargoing request number " + str(reqNumber) + " for " + request["title"] + " filed by " + request["user"]
+
+            data = json.dumps({
+               'embargo': True,
+               'date_embargo': None, #Removes the embargo date to make sure it actually embargos.
+            })
+            editedRequest = requests.patch(url + 'foia/%d/' % reqNumber, headers=headers, data=data)
+
+            print "Request Embargoed."
+            if editedRequest.status_code != 200:
+                print '*In Ron Burgendy Voice:* Error? ', editedRequest.status_code, r.text
+        page += 1
+    except Exception as e:
+        print e
+        print 'Error! ', editedRequest.status_code, ": ", editdReqeust.text
