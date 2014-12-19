8e6b0baf07e1c62c0eaa2c9f33f2b1e5262644d1
Allan Lasser <lasser.allan@gmail.com>
2014-12-19 11:53:39 -0500

    improves password input

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python2
 
 import requests
+from getpass import getpass
 
 def get_api_key():
-    user = raw_input('Username? ')
-    pw = raw_input('Password? ')
+    user = raw_input('Username: ')
+    pw = getpass()
 
     r = requests.post('https://www.muckrock.com/api_v1/token-auth/', data={'username': user, 'password': pw})
 
