466b0797dd83f79233d2581aaed6562634950bb8
Michael Morisy <m@morisy.com>
2015-08-23 01:04:52 -0400

    A number of fixes
    Still throws an error if a directory already exists.

--- export_users_requests_as_files.py
+++ export_users_requests_as_files.py
@@ -11,49 +11,40 @@ headers = utils.get_headers(token)
 
 user = raw_input('Username to export (case sensitive):')
 # user = "USERNAME" #<--- Put username here. Case sensitive.
+# request_pks = [6996] #<--- To export by MR number
 
 page = 1
 next_ = url+"foia/?user="+user
 
 
-# still need to find a nice way to specify pks from console
-# request_pks = [6996]
 
 
-while next_ is not None:
-    print "URL I'm using is " + next_
+while next_ is not None: # Handling at the page level
     r = requests.get(next_, headers=headers)
-
-    print "hey rob. check out " + str(r)
     try:
         json_data = r.json()
         print 'Page %d of %d' % (page, json_data['count'] / 20 + 1)
         next_ = json_data['next']
         for request in json_data['results']:
             print "Working on request " + str(request["id"])
-            # get request first
-
             request_url = url + 'foia/%d/' % request["id"]
-#            request = requests.get(request["url"], headers=headers) #original
-            r = requests.get(request_url, headers=headers) #original
+            r2 = requests.get(request_url, headers=headers) #original
 
-            request_data = r.json()
-            # get agency second
+            request_data = r2.json()
 
-            print "Here is the agency number " + str(request["agency"])
-            agency = requests.get("https://www.muckrock.com/api_v1/agency/" + str(request["agency"]), headers=headers)
+            print "Here is the agency number " + str(request_data["agency"])
+            agency = requests.get("https://www.muckrock.com/api_v1/agency/" + str(request_data["agency"]), headers=headers)
 
             agency_data = agency.json()
             # get communications third
 
-            communications = request['communications']
+            communications = request_data['communications']
 
             if communications is None:
-                print "No communications for request #%d." % request["id"]
-            print "before"
+                print "No communications for request #%d." % request_data["id"]
 
-            if not os.path.exists(str(request["id"])): # Checks to see if the folder exists.
-                username = requests.get(url + 'user/%d/' % request['user'], headers=headers).json()['username']
+            if not os.path.exists(str(request_data["id"])): # Checks to see if the folder exists.
+                username = requests.get(url + 'user/%d/' % request_data['user'], headers=headers).json()['username']
                 print username
                 dirName = username + '_' + agency_data['name'] + '_' + request_data['tracking_id']
                 # TODO better sanitization on directory names
@@ -63,22 +54,17 @@ while next_ is not None:
                 os.makedirs(dirName)
             else:
                 print "The directory already exists. Phew."
-            print "after"
 
             for communication in communications:
-                commNum = 0
                 #print communication
                 for file in communication['files']:
-                    print "Trying to grab a file from communication " + str(commNum)
-                    url = file['ffile']
-                    split = urlparse.urlsplit(url) # grabbed from http://stackoverflow.com/questions/2795331/python-download-without-supplying-a-filename
+                    fileurl = file['ffile']
+                    split = urlparse.urlsplit(fileurl) # grabbed from http://stackoverflow.com/questions/2795331/python-download-without-supplying-a-filename
                     filename = split.path.split("/")[-1]
                     filename = str(communication["date"])+" "+filename
                     filename = filename.replace(";", "") # to sanitize it from semi-colons
                     filename = filename.replace(":", "") # to sanitize it from colons
-                    print filename
-                    #urllib.urlretrieve(url, '/'+str(request_id)+'/'+filename)
-                    urllib.urlretrieve(url, dirName + '/' + filename)
+                    urllib.urlretrieve(fileurl, dirName + '/' + filename)
 
 
                 print "Trying to grab the text from the communication"
@@ -88,6 +74,8 @@ while next_ is not None:
                 text_file = open(dirName + '/' + communication["date"] + " Communication.txt", "w+")
                 text_file.write(communicationText)
                 text_file.close()
+                print "File closed"
+
     except:
       print "There was an error of unkown origin"
 #        print r
