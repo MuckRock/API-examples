bbaacc70df0b361f62a21ce0e9a7d8a73e1b2e27
Allan Lasser <lasser.allan@gmail.com>
2016-05-09 16:56:39 -0400

    improve get_api_key() utility method
    Instead of requesting the same username and password every time,
    the method now checks for a `MUCKROCK_TOKEN` environment variable.
    If it cannot find one, it then prompts the user for their username
    and their password. Upon obtaining the token, it tells the user
    how to add the token to their environment to skip future logins.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 
+import os
 import sys
 import requests
 from getpass import getpass
@@ -7,14 +8,21 @@ from getpass import getpass
 API_URL = 'https://www.muckrock.com/api_v1/'
 
 def get_api_key():
-    user = raw_input('Username: ')
-    pw = getpass()
-    request = requests.post('https://www.muckrock.com/api_v1/token-auth/', data={'username': user, 'password': pw})
-    json = request.json()
-    if 'token' in json:
-        return json['token']
-    else:
-        return None
+    # try getting an environment variable before requesting the auth information
+    try:
+        token = os.environ['MUCKROCK_TOKEN']
+    except KeyError:
+        user = raw_input('Username: ')
+        pw = getpass()
+        response = requests.post('https://www.muckrock.com/api_v1/token-auth/', data={'username': user, 'password': pw})
+        if not response.raise_for_status():
+            data = response.json()
+            token = data['token']
+            os.environ['MUCKROCK_TOKEN'] = token
+            print '\nYour token is: %(token)s\n\nSet it for future scripts with this command:\n\nexport MUCKROCK_TOKEN="%(token)s"\n' % {
+                'token': token
+            }
+    return token
 
 def get_headers(token=None):
     if token:
@@ -29,4 +37,3 @@ def display_progress(current, total):
     percent = (current / total) * 100.00
     sys.stdout.write("\r%d%%" % percent)
     sys.stdout.flush()
-
