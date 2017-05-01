19224533d5513df9a2997a52acacf9d736cff052
Michael Morisy <m@morisy.com>
2017-04-30 23:36:18 -0400

    Error handling
    Added some basic error handling.

--- admin/import_draft_completed_agency_tasks.py
+++ admin/import_draft_completed_agency_tasks.py
@@ -12,22 +12,28 @@ headers = utils.get_headers(token)
 
 page = 1
 
-with open('sample_upload.csv') as csvfile:
+with open('agency_upload.csv') as csvfile:
     reader = unicodecsv.DictReader(csvfile)
     for row in reader:
-        agency_url = url+"agency/"+str(row["Input.agency"])
-        print agency_url
-        a = requests.get(agency_url, headers=headers)
-        agency_json = a.json()
-        print "We're looking up new agency info for " + agency_json["name"]
-        if agency_json['status'] == "pending" and agency_json['phone'] == '' and agency_json['email'] == '' and agency_json['address'] == '' and agency_json['fax'] == '' and agency_json['website'] == '':# Make sure no one has done the agency in the meantime
-            print agency_json["name"] + "<- Agency has not been done yet. Adding info"
-            print "Will be adding " + str(row["Answer.website"]) 
-            data = json.dumps({
-                'address': row["Answer.address"],
-                'email': row["Answer.email"],
-                'fax': row["Answer.fax"],
-                'phone': row["Answer.phone"],
-                'website': row["Answer.website"]
-            })
-            print "The response is: " + str(requests.patch(agency_url, headers=headers, data=data))
+        try:
+            agency_url = url+"agency/"+str(row["Input.agency"])+"/"
+            print agency_url
+            a = requests.get(agency_url, headers=headers)
+            agency_json = a.json()
+            print "We're looking up new agency info for " + agency_json["name"]
+            if agency_json['status'] == "pending" and agency_json['phone'] == '' and agency_json['email'] == '' and agency_json['address'] == '' and agency_json['fax'] == '' and agency_json['website'] == '':# Make sure no one has done the agency in the meantime
+                print "Doing the R thang"
+                r = requests.get(agency_url, headers=headers)
+                data = json.dumps({
+                    'address': row["Answer.address"],
+                    'email': row["Answer.email"],
+                    'fax': row["Answer.fax"],
+                    'phone': row["Answer.phone"],
+                    'website': row["Answer.website"]
+                })
+                print agency_url
+                requests.patch(agency_url, headers=headers, data=data)
+            else:
+                print "That agency has already been tweaked"
+        except:
+            print "Error with agency " + row["Input.agency"] + ", usually because it was deleted."
