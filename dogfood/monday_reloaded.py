fabf2d16ccf63a0097c604b9701d7da384bf2e7b
Michael Morisy <m@morisy.com>
2018-08-07 00:08:49 -0400

    refactored and actually kinda works

--- dogfood/monday_reloaded.py
+++ dogfood/monday_reloaded.py
@@ -29,52 +29,46 @@ staff_names = (
 "Michael Morisy"
 )
 
-
+class staffer_log:
+	goal1 = ""
+	goal2 = ""
+	goal3 = ""
+	goal1data = []
+	goal2data = []
+	goal3data = []
 
 def getWeeklyUpdates(staffer):
 	print "Working on the goals progress the past week for " + staffer
 	next_ = url + 'assignment-responses/?crowdsource=25&ordering=-id'
 
-	staffer_log = []
 	day = 0
-	entry = 0
+	page = 1
 	while day < days and next_ is not None:
-		print "Looking at entry " + str(entry)
 		r = requests.get(next_, headers=headers)
 		try:
-	        json = r.json()
-	        next_ = json['next']
-	        for datum in json['results']:
-				print "User is " + datum["user"]
+			json = r.json()
+			next_ = json['next']
+			for datum in json['results']:
 				if datum["user"] == staffer:
-					print "It's a match!"
-					if day = 0:
-						staffer_log["Goal #1"]["name"] = json['results'][entry]["values"][0]['value']["name"]
-						staffer_log["Goal #2"]["name"] = json['results'][entry]["values"][0]['value']["name"]
-						staffer_log["Goal #3"]["name"] = json['results'][entry]["values"][0]['value']["name"]
+					if day == 0:
+						submitter = staffer_log()
+						submitter.goal1 = [datum][0]['values'][0]['value']
+						submitter.goal2 = [datum][0]['values'][2]['value']
+						submitter.goal3 = [datum][0]['values'][4]['value']
+						submitter.goal1data = [datum["values"][1]['value']]
+						submitter.goal2data = [datum["values"][3]['value']]
+						submitter.goal3data = [datum["values"][5]['value']]
+					else:
+						submitter.goal1data.insert(0, datum["values"][1]['value'])
+						submitter.goal2data.insert(0, datum["values"][3]['value'])
+						submitter.goal3data.insert(0, datum["values"][5]['value'])
+					day += 1
+			print 'Page %d of %d' % (page, json['count'] / 10 + 1)
+			page += 1
+		except Exception as e:
+			print e
 
-	        print 'Page %d of %d' % (page, json['count'] / 10 + 1)
-	        page += 1
-	    except Exception as e:
-	        print e
-		if json['results'][entry]["user"] == staffer:
-			print "Found goal " + str(goal) + " for " + staffer + "."
-			if name in staffer_log["Goal #" + str(goal)]]:
-				print "Adding this as the goal: " + print(json['results'][entry]["values"][0]['value']["name"])
-				staffer_log["Goal #1"]["name"] = json['results'][entry]["values"][0]['value']["name"]
-				staffer_log["Goal #1"]["entries"] = [json['results'][entry]["values"][1]['value']["name"]]
-			else:
-				print "Adding this as the goal: " + print(json['results'][entry]["values"][0]['value']["name"])
-				staffer_log["Goal #1"]["name"] = json['results'][entry]["values"][0]['value']["name"]
-				staffer_log["Goal #1"]["entries"] = [json['results'][entry]["values"][1]['value']["name"]]
-			= json['results'][entry]["values"]
-			print "Confidence for " + staffer + " was " + str(staffer_log["Quarterly Goal #" + goal][day])
-			day += 1
-			entry += 1
-		else:
-			print "No goals that time"
-			entry += 1
-	return staffer_log
+	return submitter
 
 def getStats():
 	url = 'https://www.muckrock.com/api_v1/'
@@ -127,13 +121,16 @@ if os.path.isfile('check_in.csv'):
 		stats = getStats()
 		for stat in stats:
 				mondayNotes.writelines(stat[0] + " is currently at: " + str(stat[1]) + ", which has changed by " + str(stat[2]) + "\n")
-
 		mondayNotes.writelines("==================\n")
 		mondayNotes.writelines("\n")
 
 		for staffer in staff_names:
 			print "Here is what we've got on " + staffer + ":"
-			print getWeeklyUpdates(staffer)
+			staffer_data = getWeeklyUpdates(staffer)
+			print staffer_data.goal1
+			print staffer_data.goal1data
+			print staffer_data.goal2
+			print staffer_data.goal3
 		print "All done, saving file."
 		mondayNotes.close()
 else:
