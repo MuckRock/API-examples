917bd79d70bae683e8ba9d16a3cec9d8f0cd45ba
Michael Morisy <m@morisy.com>
2020-01-15 19:08:04 -0500

    Tweaks to show off more features and use files.

--- file.py
+++ file.py
@@ -5,23 +5,34 @@ import requests
 import json
 import utils
 
+import csv
+
+with open('agencies.csv', 'rb') as f:
+    reader = csv.reader(f)
+    agencies = list(reader)
+
 url = utils.API_URL
 token = utils.get_api_key()
 headers = utils.get_headers(token)
 
-# You can get the PK for the agency and jurisdiction using their API
-# The agency you select must belong to the jurisdiction you select, or you will receive an error
-USA_PK = 10
-TEST_AGENCY_PK = 248
+f = open("requestLanguage.txt","r")
+message = f.read()
 
-data = json.dumps({
-    'jurisdiction': USA_PK,
-    'agency': TEST_AGENCY_PK,
-    'title': 'API Test File Request',
-    'document_request': 'I would like the government\'s secret receipe for the world\'s best burrito',
-    })
+print message # Just to help verify that the language is what you want included.
 
-# The request will be saved as a draft if you do not have any requests left
-r = requests.post(url + 'foia/', headers=headers, data=data)
+# If you'd like to file a test request, test agency is 248. Note that this uses a request, which you can ask and we'll refund.
 
-print(r)
+for agency in agencies:
+    print "Filing with " + str(agency)
+    data = json.dumps({
+        'agency': agency,
+        'title': 'My Public Records Request',
+        'full_text': message, # Use if you don't want any MuckRock preamble/post-amble text added.
+#        'document_request': message, # use if you do want template added around your message text
+#        'attachments': ['https://muckrock.s3.us-east-1.amazonaws.com/files_static/some_attachment.pdf'],
+        'permanent_embargo': True
+#        'embargo': True
+        })
+    # The request will be saved as a draft if you do not have any requests left
+    r = requests.post(url + 'foia/', headers=headers, data=data)
+    print(r)
