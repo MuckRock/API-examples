7ada983e7beaa773fe6d361596df1ea4f73a9bfe
Michael Morisy <m@morisy.com>
2020-09-22 09:34:31 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,23 +6,23 @@ import sys
 import requests
 from getpass import getpass
 
+# Using this method of secret storing:
+# https://stackoverflow.com/questions/25501403/storing-the-secrets-passwords-in-a-separate-file
+
+try:
+    import credentials
+    token = credentials.API_key
+except Exception as e:
+    print (e)
+    token = getpass()
+    credentials_file = open("credentials.py", "w")
+    credentials_file.writelines("API_key = \"" + token +"\"")
+    credentials_file.close
+
+
 API_URL = 'https://www.muckrock.com/api_v1/'
 
 def get_api_key():
-    # try getting an environment variable before requesting the auth information
-    try:
-        token = os.environ['MUCKROCK_TOKEN']
-    except KeyError:
-        user = raw_input('Username: ')
-        pw = getpass()
-        response = requests.post('https://www.muckrock.com/api_v1/token-auth/', data={'username': user, 'password': pw})
-        if not response.raise_for_status():
-            data = response.json()
-            token = data['token']
-            os.environ['MUCKROCK_TOKEN'] = token
-            print '\nYour token is: %(token)s\n\nSet it for future scripts with this command:\n\nexport MUCKROCK_TOKEN="%(token)s"\n' % {
-                'token': token
-            }
     return token
 
 def get_headers(token=None):
