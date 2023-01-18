78dd49109702112ebab2cfba2b484060798a0da5
Michael Morisy <m@morisy.com>
2023-01-17 19:31:53 -0500

    Updating to Python 3 plus minor tweaks
    Included additional way to modify title in the instructions.

--- bulk_file.py
+++ bulk_file.py
@@ -1,20 +1,15 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -- coding: utf-8 --
 
 import requests
 import json
 import csv
 
-# Step 1: Create a file "file_with_agencies.csv" and add the agency IDs you want to file with in it, one per line. You can use 248 and 21983 as test agencies. You do not normally need to modify the following line. Filing with test agencies will deduct requests from your account, but if you reach out we can refund those requests or you can cancel them yourself within 30 minutes.
+# Step 1: Create a file "file_with_agencies.csv" and add the agency IDs you want to file with in it, one per line. You can use 248 and 21983 as test agencies. Filing with test agencies will deduct requests from your account, but once you verify the script works as expected you can cancel the requests yourself within 30 minutes from the main interface.
 
-with open('file_with_agencies.csv', 'rb') as f:
-    reader = csv.reader(f)
-    agencies = list(reader)
-
-# Step 2: Create a file "requestLanguage.txt" with the text of the request. This should be the full length of the request and include any salutations. Do not include contact information — MuckRock generates this on a per request basis. Update the below with your request's title as indicated
+# Step 2: Create a file "requestLanguage.txt" with the text of the request. This should be the full length of the request and include any salutations. Do not include contact information — MuckRock generates this on a per request basis.
 
 f = open("requestLanguage.txt","r")
-title = "Public Records Request"
 
 # Step 3: When possible, we recommend avoiding adding attachments as agencies receive requests in a variety of ways. If needed, you can define a file to attach here. Uncomment this line, update the attachment name as appropriate, and then uncomment out the line beginning ""# 'attachments':" below as well.
 
@@ -26,12 +21,16 @@ token = "1234"
 
 # Be careful: Anyone with access to your API key has full access to your account!
 
-# Step 4: Uncomment embargo settings ("'embargo': True") as appropriate. Using embargo is dependent on your plan type on MuckRock.
+# Step 4: Adjust the title you'd like used here. It will be followed by the name of the agency after.
 
-# Step 5: Execute command, python2 bulk_file.py
+title_base = "Public Records Request: "
+
+# Step 5: Uncomment embargo settings ("'embargo': True") as appropriate. Using embargo is dependent on your plan type on MuckRock.
+
+# Step 6: Execute command, python bulk_file.py
 
 
-with open('file_with_agencies.csv', 'rb') as f:
+with open('file_with_agencies.csv', 'rt') as f:
     reader = csv.reader(f)
     agencies = list(reader)
 
@@ -41,25 +40,33 @@ headers = {
         'content-type': 'application/json'
     }
 
-print "About to file this request .... /n"
-f = open("requestLanguage.txt","r")
+print("About to file this request .... /n")
+f = open("requestLanguage.txt","rt")
 message = f.read()
 
-print message
+print(message)
 
 for agency in agencies:
+
+    # Sample Structure: https://www.muckrock.com/api_v1/agency/100/
+    agency_url = url + 'agency/' + str(agency[0]) + '/'
+    print(agency_url)
+    a = requests.get(agency_url, headers=headers)
+    agency_name = a.json()['name']
+    title = title_base + agency_name
+    print(title)
     data = json.dumps({
         'agency': agency,
         'title': title,
         'full_text': message,
 #        'attachments': [attachment],
 #         'embargo': True
-        'permanent_embargo': True
+#        'permanent_embargo': True
         })
 
     # The request will be saved as a draft if you do not have any requests left
     r = requests.post(url + 'foia/', headers=headers, data=data)
     print(r)
-    print "If it says 201, succesfully filed with " + str(agency)
+    print("If it says 201, succesfully filed with " + agency_name)
     # 402 errors indicate that your request did not have enouhg requests to file, and saved it as a draft.
     # You may need to log in to MuckRock and switch the active organization to make sure you're drawing from the correct pool.
